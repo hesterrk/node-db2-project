1a32e1a00edc422872a07d0a2e7dc03114ebcaf8
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-15 18:11:02 +0000

    put and delete

--- cars-router.js
+++ cars-router.js
@@ -6,38 +6,59 @@ const db = require("./data/dbConfig");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-    try {
-      const cars = await db("cars");
-      res.json(cats);
-    } catch (error) {
-      next(error);
-    }
-  });
+  try {
+    const cars = await db("cars");
+    res.json(cats);
+  } catch (error) {
+    next(error);
+  }
+});
 
-  router.get("/:id", async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const car = await db("cars")
-        .where({ id })
-        .first();
-      res.json(car);
-    } catch (error) {
-      next(err);
-    }
-  });
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const car = await db("cars")
+      .where({ id })
+      .first();
+    res.json(car);
+  } catch (error) {
+    next(err);
+  }
+});
 
-  router.post("/", async (req, res, next) => {
-    try {
-      const carData = req.body;
-      const [id] = await db("cars").insert(carData);
-      const newCar = await db("cars").where({ id });
-      res.status(201).json(newCar);
-    } catch (error) {
-      next(err);
-    }
-  });
+router.post("/", async (req, res, next) => {
+  try {
+    const carData = req.body;
+    const [id] = await db("cars").insert(carData);
+    const newCar = await db("cars").where({ id });
+    res.status(201).json(newCar);
+  } catch (error) {
+    next(err);
+  }
+});
 
+router.put("/:id", async (req, res, next) => {
+  try {
+    const carData = req.body;
+    await db("cars")
+      .where("id", req.params.id)
+      .update(carData);
+    const car = await db("cars").where("id", req.params.id);
+    res.json(car);
+  } catch (error) {
+    next(error);
+  }
+});
 
-
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await db("cars")
+      .where("id", req.params.id)
+      .del();
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
