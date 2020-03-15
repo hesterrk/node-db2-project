2f748b8be927d57bcb8f13aee08fe770f788dbf6
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-15 18:25:36 +0000

    added seed data

--- seeds/001-cars.js
+++ seeds/001-cars.js
@@ -1,13 +1,17 @@
+exports.seed = async function(knex) {
+  await knex("cars").truncate();
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  await knex("cars").insert([
+    { VIN: "GIO4EVJ23", make: "Nissan", model: "Fiesta", mileage: "100" },
+    { VIN: "E32HG0EIS", make: "Toyato", model: "Beetle", mileage: "200" },
+    { VIN: "XNVJSKSIW", make: "Renault", model: "Mulsanne", mileage: "400" },
+    {
+      VIN: "PDKNDKW56",
+      make: "Mercedes",
+      model: "Continental",
+      mileage: "700"
+    },
+    { VIN: "WSVDHD46S", make: "Volvo", model: "SClass", mileage: "150" },
+    { VIN: "PWIDURBVI", make: "Peugeot", model: "Flying Spur", mileage: "467" }
+  ]);
 };
