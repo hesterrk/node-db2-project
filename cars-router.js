5cd9edfd31c9a6ad8228f70d20209394566c5bde
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-15 18:05:11 +0000

    written create and read endpoints

--- cars-router.js
+++ cars-router.js
@@ -1,9 +1,41 @@
 const express = require("express");
 
-//KNEX here
+//Knex instance:
 const db = require("./data/dbConfig");
 
 const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+    try {
+      const cars = await db("cars");
+      res.json(cats);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.get("/:id", async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const car = await db("cars")
+        .where({ id })
+        .first();
+      res.json(car);
+    } catch (error) {
+      next(err);
+    }
+  });
+
+  router.post("/", async (req, res, next) => {
+    try {
+      const carData = req.body;
+      const [id] = await db("cars").insert(carData);
+      const newCar = await db("cars").where({ id });
+      res.status(201).json(newCar);
+    } catch (error) {
+      next(err);
+    }
+  });
 
 
 
